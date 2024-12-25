af8a4764e80a2844f742a5bcf163c4318062a5e0
BeatPlay <113635204+BeatPlay@users.noreply.github.com>
2024-12-25 11:22:47 +0300

    Добавьте файлы проекта.

---
+++ NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApi.Models;
+using NotesApi.Services;
+
+namespace NotesApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NotesController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Note>> Get()
+        {
+            return Ok(_noteService.GetAll());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Note note)
+        {
+            _noteService.Add(note);
+            return CreatedAtAction(nameof(Get), new { id = note.Id }, note);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _noteService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ NotesAPI/Services/NoteService.cs
@@ -0,0 +1,36 @@
+﻿using NotesApi.Models;
+
+namespace NotesApi.Services
+{
+    public interface INoteService
+    {
+        IEnumerable<Note> GetAll();
+        Note GetById(int id);
+        void Add(Note note);
+        void Delete(int id);
+    }
+
+    public class NoteService : INoteService
+    {
+        private readonly List<Note> _notes = new();
+
+        public IEnumerable<Note> GetAll() => _notes;
+
+        public Note GetById(int id) => _notes.FirstOrDefault(n => n.Id == id);
+
+        public void Add(Note note)
+        {
+            note.Id = _notes.Any() ? _notes.Max(n => n.Id) + 1 : 1;
+            _notes.Add(note);
+        }
+
+        public void Delete(int id)
+        {
+            var note = GetById(id);
+            if (note != null)
+            {
+                _notes.Remove(note);
+            }
+        }
+    }
+}
